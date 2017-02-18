249025e06021a0912633855348371a7bd25c4a55
Karl Floersch <karl@karlfloersch.com>
2017-02-18 16:45:23 +0100

    Move bet & view into different files

---
+++ bet.py
@@ -0,0 +1,121 @@
+# the Bet class implements a DAG of bets...
+# ...every bet has a sender, an estimate, and a justification (which is itself
+# a set of bets or the empty set)
+
+from settings import VALIDATOR_NAMES, ESTIMATE_SPACE
+
+bet_number = 0
+
+
+class Bet:
+    # we're now going to define the bet constructor (__init__), equality realtion (__eq__), string
+    # serialization (__str__), and hash (__hash__)...
+    # ...these things are not very conceptually important...
+
+    # constructor!
+    def __init__(self, estimate, justification, sender):
+
+        # be safe. type check!...
+        assert sender in VALIDATOR_NAMES, "...expected a validator!"
+        assert estimate in ESTIMATE_SPACE, "...expected an estimate!"
+        for b in justification:  # anything iterable, containing bets
+            assert isinstance(b, Bet), "...expected there to be only bets in the justification!"
+
+        # ...then do some assignment
+        self.sender = sender
+        self.estimate = estimate
+        self.justification = set()
+        for b in justification:
+            self.justification.add(b)
+
+        # ...sorting some things out just for debugging please ignore this!
+        global bet_number
+        self.id_number = "validator " + str(sender) + ", bet_num " + str(bet_number)
+        bet_number += 1
+
+    # equality check
+    def __eq__(self, B):
+
+        # the empty set is not a bet!
+        # this saves us from doing if isinstance(b,Bet) and b == set() every time we need to check if we're
+        # holding a set() instead of a Bet
+        if B is None:
+            return False
+
+        # ...but first be safe. type check!
+        assert isinstance(B, Bet), "Expected bet as parameter in `=='"
+
+        # two bets are equal only if they have the same estimate, sender and justification...
+        # ...it turns out that __eq__ in python's set() uses __eq__ of its members if available, under the hood...
+        # ...so this turns out to be a recursive definition! yay, recursion!
+        if self.estimate != B.estimate \
+            or self.sender != B.sender \
+                or B.justification != self.justification:
+            return False
+        return True
+
+    # this serializes bets nicely, like this: "(1, {(1, {}, 0)}, 1)"...!
+    def __str__(self):
+        string = "("
+        string += str(self.estimate) + ", {"
+        i = 0
+        # if this following line of code sometimes produces different orders (justification is a set), then
+        # we have an issue. It would be good practice to give a standard for ordering bets in justifications.
+        for b in self.justification:
+            string += str(b)
+            i += 1
+            if i != len(self.justification):  # getting fancy; leaving out commas without successive terms
+                string += ", "
+        string += "}, " + str(self.sender) + ")"
+        return string
+
+    # it turns out that to make a set of something in Python, it needs to be hashable!...
+    # ...btw it would be cool to show that for two bets A, B with A == B implies str(A) == str(B),..
+    # ...which more trivially implies hash(A) == hash(B)...
+    # ...so that we don't need to think about counterintuive Python set() behaviour...
+    # ...for example if we have set([A]).add(B) = set([A,B]) with hash(A) != hash(B) even though A == B!
+    def __hash__(self):
+        return hash(str(self))
+
+    #####################################################################################
+    # a bet A is a dependency of a bet B if either...
+    # ...A is in the justification of B...
+    # ...or A is in the dependency of bets in the justification of B!
+    #####################################################################################
+
+    # this function checks if this bet (self) is a dependency of some bet B...
+    def is_dependency(self, B):
+
+        # be safe, type check!
+        assert isinstance(B, Bet), "...expected a bet!"
+
+        # self is definitely a dependency of B if it is in the justification...
+        if self in B.justification:
+            return True
+
+        # ...or if it is in the dependency of anything in the justification!
+        for b in B.justification:
+            if self.is_dependency(b):
+                return True
+
+        # if neither of these, then "self" is not a dependency of B!
+        return False
+
+    # this one gets all the bets in the dependency of this bet (self)...
+    # ...it puts them into a set, and returns that!
+
+    # THIS NEEDS TO BE OPTIMIZED WITH MEMOIZATION TO NOT RE-TRAVERSE DAT DAG
+    def dependency(self):
+        dependencies = set()
+
+        # recurr into the justification to find all dependencies and add them to our set "d"
+        def recurr(B):
+            for b in B.justification:  # recursion bottoms on empty iterable
+                dependencies.add(b)  # note that .add in set() checks if __hash__ does not already appear!
+                recurr(b)
+
+        # now we're calling it:
+        recurr(self)
+
+        # we did it!
+        return dependencies
---
+++ view.py
@@ -0,0 +1,190 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import pylab
+
+from bet import Bet
+from settings import NUM_VALIDATORS, VALIDATOR_NAMES, ESTIMATE_SPACE, WEIGHTS
+
+
+# Views are sets of bets...
+# ...with corresponding class functions!
+class View:
+    def __init__(self, bets):
+
+        # be safe, type check!
+        for b in bets:
+            assert isinstance(b, Bet), "...expected only bets in view"
+
+        # now for some assignment...
+        self.bets = set()
+        for b in bets:
+            self.bets.add(b)
+
+    # this "serialization" has a new line for every serialization of bets...
+    # ...so that it literally looks just like this...!
+    # View:
+    # (1, {(1, {}, 0)}, 1)
+    # (0, {}, 0)
+    def __str__(self):
+        s = "View: \n"
+        for b in self.bets:
+            s += str(b) + "\n"
+        return s
+
+    def add_bet(self, bet):
+
+        # be safe, type check!...
+        assert isinstance(bet, Bet), "...expected to add a bet to the view"
+
+        # ...and finally, add the bet!
+        self.bets.add(bet)
+
+    # the dependency of a view inherits its definition from the dependency of a bet...
+    # ...it is union of the dependencies of the bets in the view!
+
+    # THIS CAN BE OPTIMIZED BY, INSTEAD OF RUNNING THE DEPENDENCY FUNCTION FROM THE BET
+    # CLASS FOR EVERY BET IN THE VIEW,...
+    # ...REWRITING IT SO THAT THE DAG IS NOT REDUNDANTLY TRAVERSED
+    def dependency(self):
+
+        dependencies = set()
+        for bet in self.bets:
+            dependencies = dependencies.union(bet.dependency())
+
+        return dependencies
+
+    # the "extension" of a view is the union of the bets in a view and the bets in its dependency!
+    def Extension(self):
+        return (self.dependency()).union(self.bets)
+
+    #####################################################################################
+    # if A is a dependency of B, B is causally dependent on A...
+    # ...which means that B is causally (and therefore chronologically) "later" than A...
+    # ...thus the definition of dependency lets us define and identify the latest bets in a given view...
+    # ...to reason about consensus in a view, we will need to identify the latest bets from each validator...!
+    #####################################################################################
+
+    # this algorithm encodes a map from validators to their lates bets, in a particular view...
+    # ...it returns a Python dictionary of the most recent bets, indexed by validator...
+    # ...and it stores empty set to handle key exceptions!
+    def LatestBets(self):
+
+        # here's the dictionary that we'll populate and return
+        latest_bets = dict()
+        for v in VALIDATOR_NAMES:
+            latest_bets[v] = None
+
+        # we are going to search every bet in the extension of view to be sure to find all of the latest bets...
+        # ...we'll call the bet we're currently inspecting "candidate"
+        for candidate_bet in (self.Extension()):
+
+            # we're going to be filtering first by validator
+            sender = candidate_bet.sender
+
+            # if we haven't heard anything from this validator...
+            # ...we can trivially say that the candidate is the latest bet we've seen, from this validator..
+            if latest_bets[sender] is None:
+                latest_bets[sender] = candidate_bet
+                continue  # ...and then we're totally free to go to the next candidate!
+
+            # if we already have a latest bet from this validator...
+            # ...we need to check if the candidate is "later" or "earlier" than this bet...
+            # ...and then update our record of the latest bet from this validator, if appropriate
+
+            # so if the candidate is in the dependency the latest bet...
+            # ...then the candidate earlier than that "latest bet"...
+            # ...so the candidate definitely is not the latest bet in the view...!
+            if candidate_bet.is_dependency(latest_bets[sender]):
+                continue  # to the next candidate!
+
+            # ...if the latest bet is a dependency of the candidate bet...
+            # ...then this candidate is "later" than our current "latest bet"
+            if latest_bets[sender].is_dependency(candidate_bet):
+                latest_bets[sender] = candidate_bet  # ...so we keep a record of the latest bets
+                continue
+
+            raise Exception("...did not expect any Byzantine (equivocating) validators!")
+
+        # after we filter through all of the bets in the extended view...
+        # ...we have our epic dictionary of latest bets!
+        return latest_bets
+
+    # this computes the maximum weight estimate from the latest bets in the view
+    def canonical_estimate(self):
+
+        # first, grab the latest bets...
+        latest_bets = self.LatestBets()
+
+        # now compute the scores of each estimate
+        scores = dict.fromkeys(ESTIMATE_SPACE, 0)
+        for v in VALIDATOR_NAMES:
+            if latest_bets[v] is None:
+                continue
+            else:
+                assert isinstance(latest_bets[v], Bet), "...expected only bets or the emptyset in the latest bets"
+                scores[latest_bets[v].estimate] += WEIGHTS[v]
+
+        # get the max score
+        max_score = 0
+        for e in ESTIMATE_SPACE:
+            if scores[e] > max_score:
+                max_score = scores[e]
+                max_score_estimate = e
+
+        # check that we have a max_score greater than zero:
+        # note that here we are requiring the tie-breaking property.
+        if max_score > 0:
+            return max_score_estimate
+        else:
+            raise Exception("...expected a non-empty view")
+
+    def plot_view(self, decided):
+
+        G = nx.DiGraph()
+
+        nodes = self.Extension()
+
+        for b in nodes:
+            G.add_edges_from([(b, b)])
+            for b2 in b.justification:
+                G.add_edges_from([(b2, b)])
+
+        # G.add_edges_from([('A', 'B'),('C','D'),('G','D')])
+        # G.add_edges_from([('C','F')])
+
+        print "decided", decided
+
+        def display_height(bet, i=0):
+
+            l = []
+            for b in bet.justification:
+                l.append(display_height(b, i+1))
+
+            if len(l) > 0:
+                return max(l) + 1
+            else:
+                return 0
+
+        positions = dict()
+
+        for b in nodes:
+            positions[b] = (float)(b.sender+1)/(float)(NUM_VALIDATORS+1), (display_height(b)+1)/4.
+
+        node_color_map = {}
+        for b in nodes:
+            if decided[b.sender] is True:
+                node_color_map[b] = 'green'
+            else:
+                node_color_map[b] = 'white'
+
+        color_values = [node_color_map.get(node) for node in G.nodes()]
+
+        labels = {}
+        for b in nodes:
+            labels[b] = b.estimate
+        # labels['B']=r'$b$'
+
+        nx.draw_networkx_labels(G, positions, labels, font_size=20)
+
+        nx.draw(G, positions, node_color=color_values, node_size=1500, edge_color='black', edge_cmap=plt.cm.Reds)
+        pylab.show()
