9702b726f83e46edd67892d76a7a80fe27b0c8ca
Karl Floersch <karl@karlfloersch.com>
2017-03-12 23:50:55 -0400

    Add arrows to graph

--- public/app.js
+++ public/app.js
@@ -6,6 +6,7 @@ var svg = d3.select("svg")
             .attr("height", height);
 
 var color = d3.scaleOrdinal(d3.schemeCategory20);
+var nodeRadius = 10;
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
@@ -65,32 +66,49 @@ function dragended(d) {
 }
 
 function plotGraph() {
-  var graph = plotViewData;
-  console.log(graph);
+  defs = svg.append("defs")
 
-  var link = svg.append("g")
-        .attr("class", "links")
-      .selectAll("line")
-      .data(graph.links)
-      .enter()
-      .append("line")
-        .attr("class", function(d) { return 'stage-' + d.stage; })
-        .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+	defs.append("marker")
+    .attr("id", "arrow")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 5)
+    .attr("refY", 0)
+    .attr("markerWidth", 4)
+    .attr("markerHeight", 4)
+    .attr("orient", "auto")
+    .attr("fill", "#777")
+    .append("path")
+      .attr("d", "M0,-5L10,0L0,5")
+      .attr("class","arrowHead");
+
 
   /* Define the data for the circles */
-  var elem = svg.append('g')
+  var linksContainer = svg.append("g")
+        .attr("class", "links")
+      .selectAll("line")
+      .data(plotViewData.links)
+  var linksEnter = linksContainer.enter();
+
+  var link = linksEnter.append("line")
+        .attr("class", function(d) { return 'stage-' + d.stage; })
+        .attr("stroke", "#999")
+        .attr("stroke-width", "1.5")
+        .attr('marker-end', 'url(#arrow)');
+
+  /* Define the data for the circles */
+  var nodesContainer = svg.append('g')
         .attr("class", "nodes")
       .selectAll('circle')
-      .data(graph.nodes)
-  var elemEnter = elem.enter()
+      .data(plotViewData.nodes)
+  var nodesEnter = nodesContainer.enter()
 	    .append("g")
       .attr("class", "nodes")
 
   estimateColors = ['HotPink', 'LightSkyBlue']
 
-  var node = elemEnter.append("circle")
+  var node = nodesEnter.append("circle")
       .attr("class", function(d) { return 'stage-' + d.stage; })
-      .attr("r", 10)
+      .attr("r", nodeRadius)
       .attr("fill", function(d) { return estimateColors[d.estimate]; })
       .call(d3.drag()
           .on("start", dragstarted)
@@ -98,7 +116,7 @@ function plotGraph() {
           .on("end", dragended));
 
   /* Create the text for each block */
-  var text = elemEnter.append("text")
+  var text = nodesEnter.append("text")
     .attr("class", function(d) { return 'stage-' + d.stage; })
 	  .text(function(d){return d.estimate})
 
@@ -107,11 +125,11 @@ function plotGraph() {
 
   //Add SVG Text Element Attributes
   simulation
-      .nodes(graph.nodes)
+      .nodes(plotViewData.nodes)
       .on("tick", ticked);
 
   simulation.force("link")
-      .links(graph.links);
+      .links(plotViewData.links);
 
   var test = true;
   function ticked() {
@@ -128,8 +146,8 @@ function plotGraph() {
     link
         .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr("x2", function(d) { return d.target.x + (d.source.x - d.target.x)*0.01; })
+        .attr("y2", function(d) { return d.target.y+nodeRadius*1.2; });
 
     text
         .attr("x", function(d) { return d.x; })
