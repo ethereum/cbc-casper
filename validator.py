99b6014bde1135e833dfafb142c6ff82667bfd74
Vlad Zamfir <vldzmfr@gmail.com>
2017-07-19 05:16:02 +0200

    removedimport of safety oracle and profiles from validator.py

--- validator.py
+++ validator.py
@@ -2,7 +2,6 @@ from settings import VALIDATOR_NAMES, ESTIMATE_SPACE, WEIGHTS
 from block import Block
 from view import View
 from justification import Justification
-from safety_oracle import Safety_Oracle
 
 import random as r
 r.seed()
@@ -14,7 +13,6 @@ class Validator:
 
     # The validator's state is a function of its view and name alone (along with global variables)
     # However, for performance's sake the validator also stores
-    @profile
     def __init__(self, name):
         assert name in VALIDATOR_NAMES, "expected a validator name"
         self.name = name
@@ -22,7 +20,6 @@ class Validator:
         self.decided = False
 
     # This method is the only way that a validator can receive protocol messages
-    @profile
     def receive_messages(self, messages):
         if not self.decided:
             self.view.add_messages(messages)
@@ -31,12 +28,10 @@ class Validator:
 
     # The estimator function returns the set of max weight estimates
     # This may not be a single-element set because the validator may have an empty view
-    @profile
     def estimate(self):
         return self.view.estimate()
 
     # This function returns the validator's latest message
-    @profile
     def my_latest_message(self):
         if self.name in self.view.latest_messages:
             return self.view.latest_messages[self.name]
@@ -45,7 +40,6 @@ class Validator:
 
     # The validator checks estimate safety by calling the safety oracle
     # This method also flags the validator as having decided in the case that the estimate is safe
-    @profile
     def check_estimate_safety(self, estimate):
         return False
         assert isinstance(estimate, Block), "..expected estimate to be a Block"
@@ -58,7 +52,6 @@ class Validator:
 
     # This function produces a new latest message for the validator
     # It updates the validator's latest message, estimate, view, and latest observed messages
-    @profile
     def make_new_message(self):
 
         justification = self.view.justification()
