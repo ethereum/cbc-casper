aba5e450ff5d50bbfaa0c18eeb0db061e231dbd4
naterush <narush@wharton.upenn.edu>
2017-09-03 00:13:49 -0400

    update presets to use closures to work with updated settings global vars

--- presets.py
+++ presets.py
@@ -1,16 +1,31 @@
-from settings import NUM_VALIDATORS, VALIDATOR_NAMES, WEIGHTS, REPORT_INTERVAL, NUM_MESSAGES_PER_ROUND, REPORT_SUBJECTIVE_VIEWS
+import settings as s
 import random as r
 
-#default function args defined once, which is both efficient and, if mutable, can store state
 
-def random(pairs=[[i, j] for i in xrange(NUM_VALIDATORS) for j in xrange(NUM_VALIDATORS) if not i == j]):
-    return r.sample(pairs, NUM_MESSAGES_PER_ROUND)
+def message_maker(mode):
+    if mode == "rand":
 
-def round_robin(msg=[0, 1]):
-    to_return = [[msg[0], msg[1]]]
-    msg[0] = (msg[0] + 1) % NUM_VALIDATORS
-    msg[1] = (msg[1] + 1) % NUM_VALIDATORS
-    return to_return
+        pairs=[[i, j] for i in xrange(s.NUM_VALIDATORS) for j in xrange(s.NUM_VALIDATORS) if not i == j]
+        def random():
+            return r.sample(pairs, s.NUM_MESSAGES_PER_ROUND)
 
-def full_propagation(pairs=[[i, j] for i in xrange(NUM_VALIDATORS) for j in xrange(NUM_VALIDATORS) if not i == j]):
-    return pairs
+        return random
+
+    if mode == "rrob":
+
+        msg=[0, 1]
+        def round_robin():
+            to_return = [[msg[0], msg[1]]]
+            msg[0] = (msg[0] + 1) % s.NUM_VALIDATORS
+            msg[1] = (msg[1] + 1) % s.NUM_VALIDATORS
+            return to_return
+
+        return round_robin
+
+    if mode == "full":
+
+        pairs=[[i, j] for i in xrange(s.NUM_VALIDATORS) for j in xrange(s.NUM_VALIDATORS) if not i == j]
+        def full_propagation():
+            return pairs
+
+        return full_propagation
