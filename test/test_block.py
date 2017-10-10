f7ed7a93971a03a41afeac876fdc5b8bb37edee0
naterush <narush@wharton.upenn.edu>
2017-10-10 12:32:43 -0400

    update test names to be more clear

--- test/test_block.py
+++ test/test_block.py
@@ -39,7 +39,7 @@ class TestUtils(unittest.TestCase):
 
         self.assertNotEqual(block_0, block_1)
 
-    def test_non_equality_of_copies_of_non_genesis(self):
+    def test_unique_block_creation_in_test_lang(self):
         test_string = "B0-A S1-A B1-B S0-B B0-C S1-C B1-D S0-D H0-D"
         testLang = TestLangCBC(test_string, [10, 11])
         testLang.parse()
@@ -55,14 +55,15 @@ class TestUtils(unittest.TestCase):
 
         self.assertEqual(num_equal, len(testLang.blocks))
 
-    def test_not_in_blockchain_off_genesis(self):
+    def test_is_in_blockchain__separate_genesis(self):
         s.update([10, 11])
         block_0 = Block(None, Justification(), 0)
         block_1 = Block(None, Justification(), 1)
 
         self.assertFalse(block_0.is_in_blockchain(block_1))
+        self.assertFalse(block_1.is_in_blockchain(block_0))
 
-    def test_in_blockchain(self):
+    def test_is_in_blockchain__test_lang(self):
         test_string = "B0-A S1-A B1-B S0-B B0-C S1-C B1-D S0-D H0-D"
         testLang = TestLangCBC(test_string, [11, 10])
         testLang.parse()
