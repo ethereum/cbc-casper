ea62306686c8c141697a063098d2c2362dbd6e5b
Danny Ryan <dannyjryan@gmail.com>
2017-10-25 12:31:04 -0500

    [feat/experiments] create experiment class

--- experiments/orphan_rate.py
+++ experiments/orphan_rate.py
@@ -2,13 +2,25 @@ import argparse
 
 from numpy import mean
 
-from simulations.analyzer import Analyzer
-from simulations.simulation_runner import SimulationRunner
+from simulations.experiment import Experiment
 from simulations.utils import (
     generate_random_validator_set,
-    message_maker,
     MESSAGE_MODES
 )
+
+
+def validator_set_generator():
+    num_validators = 5
+    mu = 100
+    sigma = 20
+    min_weight = 20
+
+    return generate_random_validator_set(
+        num_validators,
+        mu,
+        sigma,
+        min_weight
+    )
 
 
 def main():
@@ -21,50 +33,33 @@ def main():
 
     args = parser.parse_args()
 
-    num_simulations = 20
+    num_simulations = 5
     rounds_per_sim = 100
-    num_validators = 5
-    mu = 100
-    sigma = 20
-    min_weight = 20
+    report_interval = 20
 
-    analyzer_data = {
-        "orphan_rate": [],
-        "num_messages": [],
-        "num_safe_messages": []
-    }
+    data = [
+        "orphan_rate",
+        "num_messages",
+        "num_safe_messages"
+    ]
 
-    print("Running", end='')
-    for i in range(num_simulations):
-        validator_set = generate_random_validator_set(
-            num_validators,
-            mu,
-            sigma,
-            min_weight
-        )
-        runner = SimulationRunner(
-            validator_set,
-            message_maker(args.mode),
-            total_rounds=rounds_per_sim,
-        )
-        runner.run()
-        analyzer = Analyzer(runner)
-        for method in analyzer_data:
-            analyzer_data[method].append(getattr(analyzer, method)())
+    experiment = Experiment(
+        data,
+        num_simulations,
+        validator_set_generator,
+        args.mode,
+        rounds_per_sim,
+        report_interval
+    )
 
-        print(".", end='', flush=True)
+    experiment.run()
 
-    print(" complete!")
-
-    orphan_rates = analyzer_data["orphan_rate"]
-    num_messages = analyzer_data["num_messages"]
-    num_safe_messages = analyzer_data["num_safe_messages"]
-
-    print("min orphan rate:\t{}".format(min(orphan_rates)))
-    print("max orphan rate:\t{}".format(max(orphan_rates)))
-    print("avg orphan_rate:\t{}".format(mean(orphan_rates)))
-    print("total blocks:\t\t{}".format(int(mean(num_messages))))
-    print("avg safe blocks:\t{}".format(mean(num_safe_messages)))
+    final_data = experiment.final_data
+    print("min orphan rate:\t{}".format(min(final_data["orphan_rate"])))
+    print("max orphan rate:\t{}".format(max(final_data["orphan_rate"])))
+    print("avg orphan_rate:\t{}".format(mean(final_data["orphan_rate"])))
+    print("total blocks:\t\t{}".format(int(mean(final_data["num_messages"]))))
+    print("avg safe blocks:\t{}".format(mean(final_data["num_safe_messages"])))
 
 
 if __name__ == '__main__':
---
+++ simulations/experiment.py
@@ -0,0 +1,77 @@
+from simulations.analyzer import Analyzer
+from simulations.simulation_runner import SimulationRunner
+from simulations.utils import (
+    message_maker,
+)
+
+
+class Experiment:
+    def __init__(
+        self,
+        data,
+        num_simulations,
+        validator_set_generator,
+        msg_mode,
+        sim_rounds,
+        sim_report_interval
+    ):
+        self.data = data
+        self.num_simulations = num_simulations
+        self.validator_set_generator = validator_set_generator
+        self.msg_mode = msg_mode
+        self.sim_rounds = sim_rounds
+        self.sim_report_interval = sim_report_interval
+
+        self.sim_number = 0
+        self.analyzer_data = {}
+        self.final_data = {}
+
+    def run(self):
+        print("Running", end='')
+        while self.sim_number < self.num_simulations:
+            self.run_sim(self.sim_number)
+            self.sim_number += 1
+            print(".", end='', flush=True)
+
+        print(" complete!")
+
+        self._collect_final_data()
+
+
+    def run_sim(self, sim_id):
+        validator_set = self.validator_set_generator()
+        runner = SimulationRunner(
+            validator_set,
+            message_maker(self.msg_mode),
+            total_rounds=self.sim_rounds,
+            report_interval=self.sim_report_interval
+        )
+        for interval in range(int(runner.total_rounds / runner.report_interval)):
+            for step in range(runner.report_interval):
+                runner.step()
+
+            self._collect_data(runner, sim_id, interval)
+
+        self._collect_data(runner, sim_id, "final")
+
+    def aggregate_data_about_interval(self, interval):
+        return {
+            d: [
+                self.analyzer_data[sim_id][interval][d]
+                for sim_id in self.analyzer_data
+            ]
+            for d in self.data
+        }
+
+    def _collect_data(self, runner, sim_id, interval):
+        if sim_id not in self.analyzer_data:
+            self.analyzer_data[sim_id] = {}
+
+        analyzer = Analyzer(runner)
+        self.analyzer_data[sim_id][interval] = {
+            d: getattr(analyzer, d)()
+            for d in self.data
+        }
+
+    def _collect_final_data(self):
+        self.final_data = self.aggregate_data_about_interval("final")
