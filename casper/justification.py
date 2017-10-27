4cb36d389a8dbcffb707fe8dca0a69bb65ede768
naterush <narush@wharton.upenn.edu>
2017-10-27 17:31:00 -0400

    removed unused last_finalized_block from justification

--- casper/justification.py
+++ casper/justification.py
@@ -1,8 +1,7 @@
 """The justification module ..."""
 class Justification:
     """The justification class ..."""
-    def __init__(self, last_finalized_block=None, latest_messages=dict()):
-        self.last_finalized_block = last_finalized_block
+    def __init__(self, latest_messages=dict()):
         self.latest_messages = dict()
         for validator in latest_messages:
             self.latest_messages[validator] = latest_messages[validator]
