f634506dde346bddb09cbde8bb32264e1bf83b44
Danny Ryan <dannyjryan@gmail.com>
2017-10-29 18:27:22 -0500

    [chore/protocol-testing] test against generic message

---
+++ tests/casper/test_message.py
@@ -0,0 +1,35 @@
+import copy
+
+from casper.justification import Justification
+from casper.message import Message
+from casper.validator import Validator
+
+
+def test_new_message(validator):
+    message = Message(None, Justification(), validator)
+
+    assert message.sender == validator
+    assert message.estimate is None
+    assert not message.justification.latest_messages
+    assert message.sequence_number == 0
+
+
+def test_equality_of_copies_off_genesis(validator):
+    message = Message(None, Justification(), validator)
+
+    shallow_copy = copy.copy(message)
+    deep_copy = copy.deepcopy(message)
+
+    assert message == shallow_copy
+    assert message == deep_copy
+    assert shallow_copy == deep_copy
+
+
+def test_non_equality_of_copies_off_genesis():
+    validator_0 = Validator("v0", 10)
+    validator_1 = Validator("v1", 11)
+
+    message_0 = Message(None, Justification(), validator_0)
+    message_1 = Message(None, Justification(), validator_1)
+
+    assert message_0 != message_1
