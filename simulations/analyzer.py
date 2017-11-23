0c30ddae926e2464267d5ab76ca2b9db192563b0
Danny Ryan <dannyjryan@gmail.com>
2017-11-23 10:20:50 -0600

    [analyzer-properties] make analyzable methods of Analyzer properties

--- simulations/analyzer.py
+++ simulations/analyzer.py
@@ -8,40 +8,49 @@ class Analyzer:
         self.simulation = simulation
         self.global_view = simulation.network.global_view
 
+    @property
     def num_messages(self):
         return len(self.messages())
 
+    @property
     def num_safe_messages(self):
         return len(self.safe_messages())
 
+    @property
     def num_unsafe_messages(self):
         return len(self.unsafe_messages())
 
+    @property
     def num_bivalent_messages(self):
         return len(self.bivalent_messages())
 
+    @property
     def prop_safe_messages(self):
-        return float(self.num_safe_messages()) / self.num_messages()
+        return float(self.num_safe_messages) / self.num_messages
 
+    @property
     def safe_to_tip_length(self):
-        return self.global_view.estimate().height - self.safe_tip_height()
+        return self.global_view.estimate().height - self.safe_tip_height
 
+    @property
     def safe_tip_height(self):
-        if self.safe_tip():
-            return self.safe_tip().height
+        if self.safe_tip:
+            return self.safe_tip.height
         return 0
 
+    @property
     def bivalent_message_depth(self):
         max_height = max(
             message.height
             for message in self.global_view.latest_messages.values()
         )
-        return max_height - self.safe_tip_height()
+        return max_height - self.safe_tip_height
 
+    @property
     def bivalent_message_branching_factor(self):
         to_check = set(self.bivalent_messages())
-        if self.safe_tip():
-            to_check.add(self.safe_tip())
+        if self.safe_tip:
+            to_check.add(self.safe_tip)
 
         check = to_check.pop()
         branches = 0
@@ -58,8 +67,31 @@ class Analyzer:
             return 0
         return branches / num_checked
 
+    @property
     def safe_tip(self):
         return self.global_view.last_finalized_block
+
+    @property
+    def latency_to_finality(self):
+        safe_messages = self.safe_messages()
+
+        if not any(safe_messages):
+            return None
+
+        individual_latency = [
+            self.global_view.when_finalized[message] - self.global_view.when_added[message]
+            for message in safe_messages
+        ]
+
+        return statistics.mean(individual_latency)
+
+    @property
+    def orphan_rate(self):
+        num_unsafe_messages = self.num_unsafe_messages
+        num_safe_messages = self.num_safe_messages
+        if num_unsafe_messages + num_safe_messages == 0:
+            return 0
+        return float(num_unsafe_messages) / (num_unsafe_messages + num_safe_messages)
 
     def messages(self):
         return self.global_view.messages
@@ -81,30 +113,10 @@ class Analyzer:
     def unsafe_messages(self):
         potential = self.messages() - self.safe_messages()
 
-        if not self.safe_tip():
+        if not self.safe_tip:
             return set()
 
         return {
             message for message in potential
-            if message.height <= self.safe_tip().height
+            if message.height <= self.safe_tip.height
         }
-
-    def latency_to_finality(self):
-        safe_messages = self.safe_messages()
-
-        if not any(safe_messages):
-            return None
-
-        individual_latency = [
-            self.global_view.when_finalized[message] - self.global_view.when_added[message]
-            for message in safe_messages
-        ]
-
-        return statistics.mean(individual_latency)
-
-    def orphan_rate(self):
-        num_unsafe_messages = self.num_unsafe_messages()
-        num_safe_messages = self.num_safe_messages()
-        if num_unsafe_messages + num_safe_messages == 0:
-            return 0
-        return float(num_unsafe_messages) / (num_unsafe_messages + num_safe_messages)
