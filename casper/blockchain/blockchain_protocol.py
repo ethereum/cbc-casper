bfe9b394f0f5a52321af53d9abf5ce970d23e6ce
Zach Ramsay <zach.ramsay@gmail.com>
2017-11-28 02:25:57 +0000

    linting: add flake8 to CI

--- casper/blockchain/blockchain_protocol.py
+++ casper/blockchain/blockchain_protocol.py
@@ -3,6 +3,7 @@ from casper.blockchain.block import Block
 from casper.blockchain.blockchain_plot_tool import BlockchainPlotTool
 from casper.protocol import Protocol
 
+
 class BlockchainProtocol(Protocol):
     View = BlockchainView
     Message = Block
