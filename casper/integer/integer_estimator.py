1c5409cc4982c3a63133901aa2b62558829fab4d
Danny Ryan <dannyjryan@gmail.com>
2017-11-28 09:38:48 -0600

    [chore/int-cleanup] cleanup up integer estimator

--- casper/integer/integer_estimator.py
+++ casper/integer/integer_estimator.py
@@ -2,20 +2,15 @@
 
 
 def get_estimate_from_latest_messages(latest_bets):
-    """Picks the highest weight estimate (0 or 1) given some latest bets."""
+    """Picks the median weight estimate given some latest bets."""
 
-    sorted_estimates = sorted(set(latest_bets[v].estimate for v in latest_bets))
-    half_seen_weight = sum(v.weight for v in latest_bets) / 2
+    sorted_bets = sorted(latest_bets.values(), key=lambda bet: bet.estimate)
+    half_seen_weight = sum(v.weight for v in latest_bets) / 2.0
 
-    assert sum(v.weight for v in latest_bets) > 0
+    assert half_seen_weight > 0
 
     total_estimate_weight = 0
-    for _, estimate in enumerate(sorted_estimates):
-        estimate_weight = sum(
-            v.weight for v in latest_bets
-            if latest_bets[v].estimate == estimate
-        )
-
-        total_estimate_weight += estimate_weight
+    for bet in sorted_bets:
+        total_estimate_weight += bet.sender.weight
         if total_estimate_weight >= half_seen_weight:
-            return estimate
+            return bet.estimate
