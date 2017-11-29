8f2952a5bc65acc8c64441a9680459a9ad093aef
Danny Ryan <dannyjryan@gmail.com>
2017-11-29 10:11:15 -0600

    test order_estimator

--- casper/order/order_estimator.py
+++ casper/order/order_estimator.py
@@ -1,12 +1,11 @@
 def get_estimate_from_latest_messages(latest_bets):
     """Picks the highest weight estimate (0 or 1) given some latest bets."""
-
     sample_list = list(latest_bets.values())[0].estimate
     elem_weights = {elem: 0 for elem in sample_list}
-    for bet in latest_bets:
-        sender = bet.sender
+    for validator in latest_bets:
+        bet = latest_bets[validator]
         estimate = bet.estimate
         for i, elem in enumerate(estimate):
-            elem_weights[elem] += sender.weight * (len(estimate) - i)
+            elem_weights[elem] += validator.weight * (len(estimate) - i)
 
-    return sorted(elem_weights, key=lambda elem: elem_weights[elem])
+    return sorted(elem_weights, key=lambda elem: elem_weights[elem], reverse=True)
