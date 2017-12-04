f4c319a66f7d1b27e49db44a2090bdc2a88fae0f
Danny Ryan <dannyjryan@gmail.com>
2017-12-04 08:24:13 -0600

    update order_view comments

--- casper/protocols/order/order_view.py
+++ casper/protocols/order/order_view.py
@@ -1,12 +1,11 @@
-"""The blockchain view module extends a view for blockchain data structures """
+"""The order view module extends a view for ordering list data structures """
 from casper.safety_oracles.clique_oracle import CliqueOracle
 from casper.abstract_view import AbstractView
 import casper.protocols.order.order_estimator as estimator
 
 
 class OrderView(AbstractView):
-
-    """A view class that also keeps track of a last_finalized_block and children"""
+    """A view class for order consensus that also keeps track of a last_finalized_estimate"""
     def __init__(self, messages=None, first_message=None):
         super().__init__(messages)
 
